f3eb2f975347ee82f71960dadfba8e487a62b2c3
Ian Gallacher <igallacher@outlook.com>
2025-02-10 11:13:09 -0800

    Add external links to resume

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -1,3 +1,36 @@
-<div class="flex flex-col items-center w-96 pt-10 bg-slate-500">
-  <h1>Ian Gallacher</h1>
+<script lang="ts">
+  type ExternalLink = {
+    label: string;
+    href: string;
+  };
+  const externalLinks: ReadonlyArray<ExternalLink> = [
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/ian-gallacher/',
+    },
+    {
+      label: 'GitHub',
+      href: 'https://github.com/IanGallacher',
+    },
+  ];
+</script>
+
+{#snippet externalLinkDiv(externalLink: ExternalLink)}
+  <external-link>
+    <a
+      class="hover:bg-blue-500"
+      href={externalLink.href}
+    >
+      {externalLink.label}
+    </a>
+  </external-link>
+{/snippet}
+
+<div class="flex flex-col items-center w-2xl pt-10 bg-slate-500">
+  <h1 class="text-4xl">Ian Gallacher</h1>
+  <external-links class="flex flex-col gap-2">
+    {#each externalLinks as externalLink (externalLink.label)}
+      {@render externalLinkDiv(externalLink)}
+    {/each}
+  </external-links>
 </div>
